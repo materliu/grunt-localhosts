72054a53bb02c05c147e3310302219714e00b803
Filip Spiridonov <filip.spiridonov@gmail.com>
2014-09-30 21:44:42 +0300

    Insert semicolons
    It's recommended to always use semicolons in JavaScript. Have a look at the point #7 of the accepted answer: http://stackoverflow.com/questions/6950636/justifying-crockford-claims
    And this one: http://stackoverflow.com/questions/444080/do-you-recommend-using-semicolons-after-every-statement-in-javascript

--- tasks/localhosts.js
+++ tasks/localhosts.js
@@ -24,28 +24,28 @@ module.exports = function (grunt) {
      * @param  {function(err, lines)} cb
      */
     var get = function (preserveFormatting, cb) {
-        var lines = []
+        var lines = [];
         fs.createReadStream(HOSTS, 'utf8')
             .pipe(split())
             .pipe(through(function (line) {
-                var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line)
+                var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line);
                 if (matches && matches.length === 3) {
                     // Found a hosts entry
-                    var ip = matches[1]
-                    var host = matches[2]
-                    lines.push([ip, host])
+                    var ip = matches[1];
+                    var host = matches[2];
+                    lines.push([ip, host]);
                 } else {
                     // Found a comment, blank line, or something else
                     if (preserveFormatting) {
-                        lines.push(line)
+                        lines.push(line);
                     }
                 }
             }))
             .on('close', function () {
-                cb(null, lines)
+                cb(null, lines);
             })
-            .on('error', cb)
-    }
+            .on('error', cb);
+    };
 
     /**
      * Add a rule to /etc/hosts. If the rule already exists, then this does nothing.
@@ -58,23 +58,23 @@ module.exports = function (grunt) {
         get(true, function (err, lines) {
 
             // Try to update entry, if host already exists in file
-            var didUpdate = false
+            var didUpdate = false;
             lines = lines.map(function (line) {
                 if (Array.isArray(line) && line[1] === host) {
-                    line[0] = ip
-                    didUpdate = true
+                    line[0] = ip;
+                    didUpdate = true;
                 }
-                return line
-            })
+                return line;
+            });
 
             // If entry did not exist, let's add it
             if (!didUpdate) {
-                lines.push([ip, host])
+                lines.push([ip, host]);
             }
 
-            writeFile(lines, cb)
-        })
-    }
+            writeFile(lines, cb);
+        });
+    };
 
     /**
      * Remove a rule from /etc/hosts. If the rule does not exist, then this does
@@ -89,12 +89,12 @@ module.exports = function (grunt) {
 
             // Try to remove entry, if it exists
             lines = lines.filter(function (line) {
-                return !(Array.isArray(line) && line[0] === ip && line[1] === host)
-            })
+                return !(Array.isArray(line) && line[0] === ip && line[1] === host);
+            });
 
-            writeFile(lines, cb)
-        })
-    }
+            writeFile(lines, cb);
+        });
+    };
 
     /**
      * Write out an array of lines to the host file. Assumes that they're in the
@@ -106,22 +106,22 @@ module.exports = function (grunt) {
     var writeFile = function (lines, cb) {
         fs.stat(HOSTS, function (err, stat) {
             if (err) {
-                cb(err)
+                cb(err);
             } else {
-                var s = fs.createWriteStream(HOSTS, { mode: stat.mode })
-                s.on('close', cb)
-                s.on('error', cb)
+                var s = fs.createWriteStream(HOSTS, { mode: stat.mode });
+                s.on('close', cb);
+                s.on('error', cb);
 
                 lines.forEach(function (line, lineNum) {
                     if (Array.isArray(line)) {
-                        line = line[0] + ' ' + line[1]
+                        line = line[0] + ' ' + line[1];
                     }
-                    s.write(line + (lineNum === lines.length - 1 ? '' : EOL))
-                })
-                s.end()
+                    s.write(line + (lineNum === lines.length - 1 ? '' : EOL));
+                });
+                s.end();
             }
-        })
-    }
+        });
+    };
 
     grunt.registerMultiTask('localhosts', "Change your local machine hosts",
         function () {
@@ -151,8 +151,6 @@ module.exports = function (grunt) {
                             });
                             break;
                     }
-
-
                 });
             } else {
                 done();
